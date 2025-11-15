d5746dfd918c02c3811e0657af9ece2caa8a5aee
Priyesh <dev1@git.com>
2025-11-15 16:30:25 +0530

    Add API Gateway

---
+++ gateway/src/test/java/com/example/teleapp/gateway/TeleappGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teleapp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleappGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
